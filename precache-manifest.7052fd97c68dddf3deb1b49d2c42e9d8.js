1d540c76bd2dbc09de24a7f4276dfbca82a4fb4e
T Z <kazakova.tanya@gmail.com>
2020-04-22 12:24:12 +0300

    Updates

---
+++ precache-manifest.7052fd97c68dddf3deb1b49d2c42e9d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd39dd6644324f326d954019190c254f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b42964a1254b5ae57f6",
+    "url": "./static/css/main.3287f9e5.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b42964a1254b5ae57f6",
+    "url": "./static/js/main.683a3057.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
